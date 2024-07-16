9afb803c604671aa3b6eb58c81fb87326a216036
dnlKaizer <danilokaizer@gmail.com>
2024-07-16 15:46:20 -0300

    Contador de 6 na main
    Implementação dos métodos na main:
        static int[] rolar(ParDeDados parDeDados);
        static int verificarNumeroDados(int[] dados, int num);
        static int contarNumeroSeis(ParDeDados parDeDados, int nJogadas);

--- PigGame/src/Main.java
+++ PigGame/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ParDeDados parDeDados = new ParDeDados();
+        int n = 300;
+        int quantidade = contarNumeroSeis(parDeDados, n);
+        System.out.println(quantidade);
+    }
+
+    static int[] rolar(ParDeDados parDeDados) {
+        int[] dados = new int[2];
+        parDeDados.rolar();
+        dados[0] = parDeDados.getDado1();
+        dados[1] = parDeDados.getDado2();
+        return dados;
+    }
+
+    static int verificarNumeroDados(int[] dados, int num) {
+        int quantidade = 0;
+        for (int i : dados) {
+            if (i == num) {
+                quantidade++;
+            }
+        }
+        return quantidade;
+    }
+
+    static int contarNumeroSeis(ParDeDados parDeDados, int nJogadas) {
+        int quantidade = 0;
+        for (int i = 0; i < nJogadas; i++) {
+            quantidade += verificarNumeroDados(rolar(parDeDados), 6);
+        }
+        return quantidade;
     }
 }
