5d27c615513da780644406eb70996f805276a2c3
dnlKaizer <danilokaizer@gmail.com>
2024-07-16 16:22:19 -0300

    Atualização da classe Jogador

--- PigGame/src/Jogador.java
+++ PigGame/src/Jogador.java
@@ -1,7 +1,25 @@
 public class Jogador {
     private boolean statusArriscando = false;
+    private int pontos = 0;
+
+    public int getPontos() {
+        return pontos;
+    }
+    public void updatePontos(int pontos) {
+        this.pontos += pontos;
+    }
+    public void restartPontos() {
+        pontos = 0;
+    }
 
     public boolean getStatusArriscando() {
         return statusArriscando;
+    }
+    public void updateStatusArriscando() {
+        if (statusArriscando) {
+            statusArriscando = false;
+        } else {
+            statusArriscando = true;
+        }
     }
 }
