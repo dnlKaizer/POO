086ecb16be4263f7cf5e08ff4d90febf6486b84f
dnlKaizer <danilokaizer@gmail.com>
2024-07-16 15:31:06 -0300

    Criação da classe ParDeDados
    Atributos:
        int dado1
        int dado2
    
    Métodos:
        getters
        void rolar()

--- PigGame/src/ParDeDados.java
+++ PigGame/src/ParDeDados.java
@@ -1,3 +1,19 @@
+import java.util.Random;
+
 public class ParDeDados {
-    
+    private int dado1;
+    private int dado2;
+
+    public int getDado1() {
+        return dado1;
+    }
+    public int getDado2() {
+        return dado2;
+    }
+
+    public void rolar() {
+        Random random = new Random();
+        this.dado1 = random.nextInt(1, 7);
+        this.dado2 = random.nextInt(1, 7);
+    }
 }
